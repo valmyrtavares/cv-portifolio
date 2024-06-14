fc898313a3356624e07443cbcdc2abda1722e31b
valmyrtavares <valmyrtavares71@gmail.com>
2024-06-14 15:31:55 -0300

    exchanged  items of place in project , keeping the current  size of both and the proportionally

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,6 +13,7 @@ const Projects = () => {
       <section>
         <h1>Projeto CMS Angular Casa de veraneio</h1>
         <div className="project-container">
+          <img src={gif} alt="Gif do projeto" />
           <div>
             <p style={{ color: "white" }}>
               Esse é um projeto sobre uma casa de veraneio com CMS que
@@ -33,10 +34,29 @@ const Projects = () => {
               possam ser selecionadas diretamente da plataforma do usuário tanto
               no desktop quanto no mobile.
             </p>
+            <a src="www.google.com">Documentação do projeto</a>
           </div>
+          <img src={gif} alt="Gif do projeto" />
           <div>
-            {" "}
-            <img src={gif} alt="Gif do projeto" />
+            <p style={{ color: "white" }}>
+              Esse é um projeto sobre uma casa de veraneio com CMS que
+              proporciona o completo gerenciamento pelo administrador do
+              empreendimento para adicionar imagens, promoções, parcerias, redes
+              sociais no estilo wordpress.{" "}
+            </p>
+            <p>
+              Foi completamente construído com Angular e Mobile First. Aqui
+              temos uma breve apresentação das partes principais do projeto
+              facilitando o entendimento desde o design até o código de todas as
+              partes.
+            </p>
+            <p>
+              O backend foi todo desenvolvido com as ferramentas realtime e
+              storage da plataforma firebase do google, criando toda a parte
+              administrativa do usuário, fazendo com que as todas as imagens
+              possam ser selecionadas diretamente da plataforma do usuário tanto
+              no desktop quanto no mobile.
+            </p>
           </div>
         </div>
       </section>
