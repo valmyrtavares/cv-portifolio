47b91beb0fb27e05fcebdba875fc909f65e55a8a
Valmyr Tavares <valmyrtavares71@gmail.com>
2024-07-09 18:21:41 -0300

    fix text

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import React from 'react';
 import image from '../styles/images/PersonalPhoto.PNG';
 import PropTypes from 'prop-types';
 import SkillTraining from '../components/SkillTraining';
+import '../styles/pages/about.scss';
 
 const Profile = ({ language }) => {
   return (
@@ -13,40 +14,39 @@ const Profile = ({ language }) => {
             <p>
               <span>Olá,</span>{' '}
             </p>
-
             <p>
               Desenvolvedor Front End com experiência profissional nos
               principais frameworks <span>javascript/typescript</span> como
               <span> Vue.js, Angular e React</span>
             </p>
-
             <p>
               Atua no mercado desde 2018, trabalhando em correção de bugs,
               criação de telas e novas funcionalidades em vários sistemas. No
               ano de 2022 e 2023 trabalhou especialmente no ramo de
               telecomunicações na empresa Netcracker. Em 2021 e 2022 atuou em
-              sistemas de concursos públicos, que elaborava provas e organizava
-              locais de exames na empresa Fundação Carlos Chagas, entre outras
-              experiências também trabalhou na criação de E-commerces na empresa
-              Seller Face
+              sistemas de concursos públicos,na Fundação Carlos Chagas criando
+              sistemas para elaboração provas e organização de locais de exames.
+              entre outras experiências também trabalhou na criação de
+              E-commerces na empresa Seller Face
             </p>
-
             <p>
               Tem experiência em backend nas linguagens PHP e Node.js, mas
               sempre atuou como full-satck em projetos como free lancer
             </p>
-            <p>
-              " Eu acredito que todos os processos podem ser simplificados o
+            <p className="quote">
+              {' '}
+              Eu acredito que todos os processos podem ser simplificados ou
               otimizados trazendo ganho e oportunidades para toda a sociedade.
-              Exatamente por isso sou tão facinado por escrever código. Porque é
-              de forma prática o caminho onde qualquer um com boa vontade e
+              Exatamente por isso sou tão fascinado por escrever código. Porque
+              é de forma prática o caminho onde qualquer um com boa vontade e
               disciplina transforma o mundo e a vida das pessoas, saindo do
-              discursso teórico sustentado por tantos lideres e politicos ao
-              redor do mundo, com a promessas irreais. Programar é realmente ter
-              a oportunidade de transformar ideias em soluções que mudam a vida
+              discurso teórico sustentado por tantos líderes e políticos ao
+              redor do mundo, com promessas irreais. Programar é realmente ter a
+              oportunidade de transformar ideias em soluções que mudam a vida
               das pessoas, e cria um mundo senão melhor, mas mais interessante e
-              funcional "
+              funcional.
             </p>
+            <span>Valmyr Tavares</span>
           </section>
         ) : (
           <section className="about-text">
